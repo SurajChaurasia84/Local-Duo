2d5786f1bb28852d1622887515051c759ded4a2a
Suraj Chaurasia <suraj84chaurasia@gmail.com>
2026-04-19 00:14:38 +0530

    Add providers

---
+++ lib/providers/issue_provider.dart
@@ -0,0 +1,39 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../models/issue.dart';
+import '../services/api_service.dart';
+
+final apiServiceProvider = Provider((ref) => ApiService());
+
+final issuesProvider = FutureProvider<List<Issue>>((ref) async {
+  return ref.watch(apiServiceProvider).getIssues();
+});
+
+final userIssuesProvider = FutureProvider<List<Issue>>((ref) async {
+  return ref.watch(apiServiceProvider).getUserIssues();
+});
+
+class SubmitIssueNotifier extends StateNotifier<AsyncValue<void>> {
+  final ApiService _apiService;
+  SubmitIssueNotifier(this._apiService) : super(const AsyncValue.data(null));
+
+  Future<bool> submit(Issue issue) async {
+    state = const AsyncValue.loading();
+    try {
+      final success = await _apiService.createIssue(issue);
+      if (success) {
+        state = const AsyncValue.data(null);
+        return true;
+      } else {
+        state = AsyncValue.error('Failed to submit issue', StackTrace.current);
+        return false;
+      }
+    } catch (e, st) {
+      state = AsyncValue.error(e, st);
+      return false;
+    }
+  }
+}
+
+final submitIssueProvider = StateNotifierProvider<SubmitIssueNotifier, AsyncValue<void>>((ref) {
+  return SubmitIssueNotifier(ref.watch(apiServiceProvider));
+});
